9707b0cac6de21c068c2e35e492406655cd5fe4f
y123321 <yperelman@hotmail.com>
2020-04-14 00:40:08 +0300

    Add files via upload

---
+++ publish/wwwroot/src/ViewControllers/InstrumentsVC.js
@@ -0,0 +1,55 @@
+﻿import ViewManager from "../Managers/ViewManager.js";
+const defaultCollectionId = "global";
+class InstrumentsVC{
+    instrumentListSelector = ".instrumentsList";
+    static CONTAINER_TEMPLATE = ViewManager.createTemplate("instrumentsListTemplate");
+    renderInstrumentsArr(instruments, collectionId) {
+        if (!Array.isArray(instruments))
+            return null;
+        //get global instruments if no id was selected
+        if (!collectionId)
+            collectionId = defaultCollectionId;
+
+        let container = getEmpltyContainerTemplate(collectionId);
+
+        addInstrumentsToContainer(container, instruments);
+
+        return container;
+    }
+
+
+    renderInstrument(instrument) {
+        var veiwId = this.getViewId(instrument.instrumentId);
+        let view = ViewManager.renderModelObject(instrument, "instrumentTemplate", veiwId);
+        return view;
+    }
+    getViewId(instrumentId) {
+        return "instrument_" + instrumentId;
+    }
+    getViewCollectionId(collectionId) {
+        if (!collectionId)
+            collectionId = defaultCollectionId;
+        return "instrument-collection_" + collectionId;
+    }
+}
+const instance = new InstrumentsVC();
+export default instance;
+
+function addInstrumentsToContainer(container,instruments) {
+    var list = container.querySelector(instance.instrumentListSelector)
+    for (let i = 0; i < instruments.length; i++) {
+        let instrument = instruments[i];
+        if (!instrument.instrumentId)
+            continue;
+        let view = instance.renderInstrument(instrument);
+        list.appendChild(view);
+    }
+    return list;
+}
+
+function getEmpltyContainerTemplate(collectionId) {
+    var viewId = instance.getViewCollectionId(collectionId);
+    let container = InstrumentsVC.CONTAINER_TEMPLATE.cloneNode(true);
+    ViewManager.setTemplateFieldsValues(null, container, viewId);
+    return container;
+}
---
+++ publish/wwwroot/src/ViewControllers/RegisterVC.js
@@ -0,0 +1,29 @@
+﻿import ViewManager from "../Managers/ViewManager.js";
+import RegisterController from "../Controllers/RegisterController.js";
+
+class RegisterVC {
+    _registrationView=null;
+    renderRegisterPage() {
+        var view = ViewManager.renderModelObject(null, "registrationTemplate");
+        this._registrationView = view;
+        let registerBtn = view.querySelector(".btnRegister");
+        registerBtn.addEventListener("click", () => {
+            let data = ViewManager.serializeToObj(view);
+            RegisterController.register(data);
+        });
+        return view;
+    }
+    setValidationErrors(errors) {
+        if (!errors)
+            return;
+        if (this._registrationView !== null) {
+            let el = this._registrationView.querySelector(".validationErrors");
+            let msg = "";
+            for (let prop in errors) {
+                msg += prop + ": " + errors[prop]+"<br/>";
+            }
+            el.innerHTML = msg;
+        }
+    }
+}
+export default new RegisterVC();
